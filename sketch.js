afdf8da485ef4d0947341f36f134050e4211f8bf
Skylix17 <rivaldipebriawan@gmail.com>
2025-10-30 16:47:35 +0800

    membuat lingkaran bisa keatas, kebawah, kekiri, dan kekanan

--- sketch.js
+++ sketch.js
@@ -1,15 +1,25 @@
 // User defined variable
 // EVENT
-size = 60
+x = 300;
+y = 300;
+size = 80;
+
 function setup() {
   createCanvas(600, 600);
-  background(78, 19, 187);
 }
 
 function draw() {
-  fill(random(255), random(255), random(255))
-  circle(mouseX, mouseY, size);
-  if (mouseIsPressed) {
-    size -= 2;
+  background(78, 19, 187);
+  if (keyIsPressed === true) {
+    if (keyCode === UP_ARROW) {
+      y -= 4;
+    } else if (keyCode === DOWN_ARROW) {
+      y += 4;
+    } else if (keyCode === LEFT_ARROW) {
+      x -= 4;
+    } else if (keyCode === RIGHT_ARROW) {
+      x += 4;
+    }
   }
+  circle(x, y, size);
 }
