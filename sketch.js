09a29221b145eda1fda46d1c214e5e9b62508a4c
Skylix17 <rivaldipebriawan@gmail.com>
2025-10-30 16:40:55 +0800

    membuat lingkaran berwarna yang makin membesar ketika di klik mengikuti mouse/cursor

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 // User defined variable
 // EVENT
+size = 60
 function setup() {
   createCanvas(600, 600);
   background(78, 19, 187);
 }
 
 function draw() {
-  noStroke();
-  circle(mouseX, mouseY, 80);
+  fill(random(255), random(255), random(255))
+  circle(mouseX, mouseY, size);
+  if (mouseIsPressed) {
+    size -= 2;
+  }
 }
